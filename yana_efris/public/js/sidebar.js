5ebe91060f9370058e3ae0586e629ba24a733e67
Neel Brahmbhatt <neelbrahmbhatt2002@gmail.com>
2026-01-30 13:37:55 +0000

    Rename Home link to Dashboard

--- yana_efris/public/js/sidebar.js
+++ yana_efris/public/js/sidebar.js
@@ -305,7 +305,7 @@ $(document).ready(function () {
 				return;
 			}
 
-			$public_section.append(create_home_link("Home", "/app/home", "dashboard"));
+			$public_section.append(create_home_link("Dashboard", "/app/home", "dashboard"));
 
 			CUSTOM_MENUS.forEach((menu) => {
 				$public_section.append(create_menu_group(menu));
