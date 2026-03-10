d82de4cd82fa0bc62b4a3a6dc7d2ab3af1588c24
Neel Brahmbhatt <neelbrahmbhatt2002@gmail.com>
2026-03-10 09:48:19 +0000

    Solve wrong subscription error message

--- yana_efris/public/js/permission_override.js
+++ yana_efris/public/js/permission_override.js
@@ -23,7 +23,6 @@ frappe.after_ajax(function () {
 				message_text.includes("does not have doctype access") ||
 				message_text.includes("No permission for") ||
 				message_text.includes("Not permitted") ||
-				message_text.toLowerCase().includes("not found") ||
 				message_text.includes("The resource you are looking for is not available")
 			) {
 				// 🔥 Prevent duplicate popup within 1 second
