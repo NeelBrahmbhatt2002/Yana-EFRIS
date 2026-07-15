fdba305f2c94e3546a446a9149dfaaeaae64ddeb
Neel Brahmbhatt <neelbrahmbhatt2002@gmail.com>
2026-07-15 05:45:52 +0000

    Remove Purchase Dashboard link as of now

--- yana_efris/public/js/sidebar.js
+++ yana_efris/public/js/sidebar.js
@@ -92,7 +92,7 @@ const CUSTOM_MENUS = [
 		title: "Purchase",
 		icon: "buying",
 		items: [
-			{ label: "Purchase Dashboard", link: "/app/purchase-dashboard", icon: "" },
+			// { label: "Purchase Dashboard", link: "/app/purchase-dashboard", icon: "" },
 			{ label: "Suppliers", link: "/app/supplier", icon: "" },
 			{ label: "Purchase Orders", link: "/app/purchase-order", icon: "" },
 			{ label: "Purchase Invoices", link: "/app/purchase-invoice", icon: "" },
